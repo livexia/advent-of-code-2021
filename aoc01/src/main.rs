e492acbbedd3c2b3d2ed24fe0690d0f712644f3b
livexia <istillalive@live.cn>
2021-12-01 13:57:26 +0800

    finish day 1

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -10,8 +10,33 @@ type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()>{
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let depths: Vec<u32> = input.lines()
+        .map(|l| l.parse().unwrap())
+        .collect();
 
-    // part1()?;
-    // part2()?;
+    part1(&depths)?;
+    part2(&depths)?;
+    Ok(())
+}
+
+fn part1(depths: &[u32]) -> Result<()> {
+    let mut result = 0;
+    for i in 0..depths.len() - 1 {
+        if depths[i] < depths[i + 1] {
+            result += 1
+        }
+    }
+    writeln!(io::stdout(), "there are {} measurements that are larger than the previous measurement.", result)?;
+    Ok(())
+}
+
+fn part2(depths: &[u32]) -> Result<()> {
+    let mut result = 0;
+    for i in 0..depths.len() - 3 {
+        if depths[i] < depths[i + 3] {
+            result += 1
+        }
+    }
+    writeln!(io::stdout(), "there are {} sums that are larger than the previous sum..", result)?;
     Ok(())
 }
