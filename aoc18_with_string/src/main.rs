4e8a991b3ff85de41786d9d6aaba6fd1871124e3
livexia <istillalive@live.cn>
2021-12-18 23:05:44 +0800

    cleanup code and add more comments

--- aoc18_with_string/src/main.rs
+++ aoc18_with_string/src/main.rs
@@ -9,6 +9,9 @@ macro_rules! err {
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
+// 首先对字符串进行处理，将字符串转为数字，其中']'、'['、','，分别用-1，-2，-3来代表。
+// 输出时则是将数字重新转为字符打印输出。
+
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
@@ -58,12 +61,12 @@ pub fn convert(s: &str) -> Vec<i32> {
 
 pub fn convert_back(s: &[i32]) -> String {
     let mut result = String::new();
-    for c in s {
+    for &c in s {
         match c {
             -1 => result.push('['),
             -2 => result.push(']'),
             -3 => result.push(','),
-            c => result.push_str(&format!("{}", c)),
+            c => result.push_str(&c.to_string()),
         }
     }
     result
@@ -120,6 +123,8 @@ pub fn explode(s: Vec<i32>) -> Vec<i32> {
     // let max_height = height(&s);
     let mut height = 0;
     let mut stack = s.clone();
+
+    // 寻找需要explode的pair对，并不直接修改，记录位置
     for (i, c) in s.into_iter().enumerate() {
         if c == -1 {
             height += 1;
@@ -128,7 +133,7 @@ pub fn explode(s: Vec<i32>) -> Vec<i32> {
         } else if c >= 0 {
             // if height >= max_height && pair.len() < 2 {
             if height > 4 && pair.len() < 2 {
-                pair.push((i, c));
+                pair.push(i);
             }
             if pair.len() == 2 {
                 break;
@@ -136,47 +141,55 @@ pub fn explode(s: Vec<i32>) -> Vec<i32> {
         }
     }
 
-    let mut left_number_index = 0;
-    for i in (0..pair[0].0).rev() {
+    // 寻找并修改距离需要explode最近的pair的常规数字。
+    // 注意在此并不对stack进进行删除操作，不影响记录的pair对位置。
+    // 避免对位置的复杂计算，同时防止删除操作对后续值的影响。
+
+    // 寻找并修改左侧最近的常规数字
+    for i in (0..pair[0]).rev() {
         if stack[i] >= 0 {
-            let value = pair[0].1 + stack[i];
+            let value = stack[pair[0]] + stack[i];
             stack[i] = value;
-            left_number_index = i;
             break;
         }
     }
-    let mut right_number_index = 0;
-    for i in pair[1].0 + 1..stack.len() {
+
+    // 寻找并修改右侧最近的常规数字
+    for i in pair[1] + 1..stack.len() {
         if stack[i] >= 0 {
-            let value = pair[1].1 + stack[i];
+            let value = stack[pair[1]] + stack[i];
             stack[i] = value;
-            right_number_index = i;
             break;
         }
     }
-    let end;
-    let start;
 
-    if left_number_index == 0 {
-        end = pair[0].0;
-        stack[pair[0].0 - 1] = 0;
-        start = pair[1].0 + 2
-    } else if right_number_index == 0 {
-        start = pair[1].0 + 1;
-        stack[pair[1].0 + 1] = 0;
-        end = pair[0].0 - 1
-    } else {
-        stack[pair[0].0 - 1] = 0;
-        end = pair[0].0;
-        start = pair[1].0 + 2;
-    }
+    // 考虑每一次explode，考虑最内层次的情况，例如[x,y]。
+    // [x,y] 必然位于一个pair中的左边或者右边。
+    // explode后[x,y]会变成0，无论在左边还是右边有没有常规的数字。
+    // 因为前面已经寻找了x和y可能的增加位置（左边和右边可能的最近的常规数字位置）。
+    // 我们只需要把[x,y]中x的前一个位置也就是[改为0，即 stack[pair[1].0 - 1] = 0。
+    // 从0到包含这个位置的所有字符是新的数字的开始。
+    // 也是就是stack[..pair[0].0 + 1]或者stack[..=pair[0].0]
+    // 新数字的剩余部分，就是从不包含y的后一个位置开始到结尾。
+    // 也就是stack[pair[1].0 + 2..] 
+    // 当然这只是其中一种参考方式，同样的也可以把y之后的一个位置也就是]置0；
+    // 然后新的数字从0到x之前的一个位置为止，不包含x的前一个位置。
+    // 剩余的数字则是从y之后的一个位置开始到结尾，包含y之后的一个位置。
+    // stack[pair[1].0 + 1] = 0;
+    // let start = pair[1].0 + 1;
+    // let end = pair[0].0 - 1;
 
+    stack[pair[0] - 1] = 0;
+    let end = pair[0];
+    let start = pair[1] + 2;
+
+    // 根据取得的end位置和start位置对stack进行slice和concat操作，就得到了新的数字
     [&stack[..end], &stack[start..]].concat()
 }
 
 pub fn split(s: Vec<i32>) -> Vec<i32> {
     let mut stack = vec![];
-    let mut flag = false;
+    let mut flag = false; // 记录split次数，保证只进行一次split
     for c in s {
         if c > 9 && !flag {
             stack.push(-1);
