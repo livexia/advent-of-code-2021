588dbc3fed59402ed34a85ec897ec012101504e2
livexia <istillalive@live.cn>
2021-12-22 18:45:44 +0800

    finish day 22

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::io::{self, Read, Write};
-use std::collections::HashSet;
 use std::str::FromStr;
 
 macro_rules! err {
@@ -11,253 +11,136 @@ type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
 type Coord = (i64, i64, i64);
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
     // step (bool, (i64, i64), (i64, i64), (i64, i64))
-    let steps: Vec<Step> = input.lines()
-        .map(|s| s.parse()).collect::<Result<Vec<Step>>>()?;
+    let cubes: Vec<Cube> = input
+        .lines()
+        .map(|s| s.parse())
+        .collect::<Result<Vec<Cube>>>()?;
 
-    writeln!(io::stdout(), "there is {} steps", steps.len())?;
+    writeln!(io::stdout(), "there is {} steps", cubes.len())?;
 
-    part1(&steps)?;
-    part2_brute_force(&steps)?;
-    // part2(&steps)?;
+    part1(&cubes)?;
+    part2(&cubes)?;
     Ok(())
 }
 
-fn part2(steps: &Vec<Step>) -> Result<()> {
-    // the step inside the region x=-50..50,y=-50..50,z=-50..50
-    // those step do not overlap with other steps
+fn part1(cubes: &Vec<Cube>) -> Result<()> {
+    let init_cube = Cube {
+        state: true,
+        x: (-50, 50),
+        y: (-50, 50),
+        z: (-50, 50),
+    };
+    let sub_cubes = cubes
+        .iter()
+        .filter_map(|c| c.sub_cube(&init_cube))
+        .collect::<Vec<Cube>>();
+    let result = calc_volume(&sub_cubes);
+    writeln!(
+        io::stdout(),
+        "Part1: ther is {} cubes are on the initialization procedure region",
+        result
+    )?;
+    Ok(())
+}
 
-    let mut count = 0;
+fn part2(cubes: &Vec<Cube>) -> Result<()> {
+    let result = calc_volume(&cubes);
+    writeln!(io::stdout(), "Part2: there is {} cubes", result)?;
+    Ok(())
+}
 
-    let start = 0;
-
-    let mut stacks = vec![steps[start].clone()];
-
-    for other in &steps[start + 1..] {
-        let mut new_stacks = vec![];
-        for step in &stacks {
-            let i1 = step.intersection_with(other);
-            let i2 = other.intersection_with(step);
-            if i1.len() == 0 && i2.len() == 0 {
-                new_stacks.push(step.clone());
-                new_stacks.push(other.clone());
-            } else if i1.len() == 1 && i2.len() == 1 {
-                if !step.state && !other.state {
-                    new_stacks.push(step.clone())
+fn calc_volume(cubes: &[Cube]) -> i64 {
+    let mut stack = vec![cubes[0].clone()];
+    for next_cube in &cubes[1..] {
+        let mut new_stack = vec![];
+        for cube in &stack {
+            new_stack.push(cube.clone());
+            if let Some(mut sub_cube) = cube.sub_cube(next_cube) {
+                if cube.state == next_cube.state {
+                    sub_cube.state = !next_cube.state;
                 } else {
-                    let state = if step.state && other.state {
-                        false
-                    } else if step.state && !other.state {
-                        false
-                    } else if !step.state && other.state {
-                        true
-                    } else {
-                        unreachable!("Should be unreachable")
-                    };
-                    let p1 = i1[0];
-                    let p2 = i2[0];
-                    let new_step = Step {
-                        state,
-                        min_x: p1.0.min(p2.0),
-                        max_x: p1.0.max(p2.0),
-                        min_y: p1.1.min(p2.1),
-                        max_y: p1.1.max(p2.1),
-                        min_z: p1.2.min(p2.2),
-                        max_z: p1.2.max(p2.2),
-                    };
-                    new_stacks.push(step.clone());
-                    new_stacks.push(new_step);
-                    if other.state {
-                        new_stacks.push(other.clone());
-                    }
+                    sub_cube.state = next_cube.state;
                 }
-            } else if i1.len() == 2 && i2.len() == 0 {
-    
-            } else if i1.len() == 0 && i2.len() == 2 {
-    
-            } else {
-                return err!("Existence of situations not considered, {}, {}", i1.len(), i2.len());
-            }
-            println!("{}, {}", i1.len(), i2.len());
-        }
-        stacks = new_stacks;
-    }
-    println!("{:?}", stacks);
-    let mut count = 0;
-    for step in stacks {
-        let sign = if step.state {
-            1
-        } else {
-            -1
-        };
-        count += sign * step.count();
-    }
-    println!("{:?}", count);
-    Ok(())
-}
-
-
-fn part2_brute_force(steps: &Vec<Step>) -> Result<()> {
-    let mut on_cubes: HashSet<(i64, i64, i64)> = HashSet::new();
-    let mut off_cubes: HashSet<(i64, i64, i64)> = HashSet::new();
-    for step in steps.iter().rev() {
-        for x in step.min_x..=step.max_x {
-            for y in step.min_y..=step.max_y {
-                for z in step.min_z..=step.max_z {
-                    if on_cubes.contains(&(x, y, z)) || off_cubes.contains(&(x, y, z)) {
-                        continue;
-                    }
-                    if step.state {
-                        on_cubes.insert((x, y, z));
-                    } else {
-                        off_cubes.insert((x, y, z));
-                    }
-                }
+                new_stack.push(sub_cube);
             }
         }
-    }
-    writeln!(io::stdout(), "Part2: {}", on_cubes.len())?;
-    Ok(())
-}
-
-fn part1(steps: &Vec<Step>) -> Result<()> {
-    let steps: Vec<Step> = steps.iter().map(|s| s.part1_step()).collect();
-    let mut on_cubes: HashSet<(i64, i64, i64)> = HashSet::new();
-    for step in steps {
-        let next_state = step.state;
-        for x in step.min_x..=step.max_x {
-            for y in step.min_y..=step.max_y {
-                for z in step.min_z..=step.max_z {
-                    let last_state = on_cubes.contains(&(x, y, z));
-                    if next_state {
-                        on_cubes.insert((x, y, z));
-                    }
-                    if last_state && !next_state {
-                        on_cubes.remove(&(x, y, z));
-                    }
-                }
-            }
+        if next_cube.state {
+            new_stack.push(next_cube.clone());
         }
+        stack = new_stack;
+        println!("{}", stack.len());
     }
-    writeln!(io::stdout(), "Part1: ther is {} cubes are on the initialization procedure region", on_cubes.len())?;
-    Ok(())
+    stack
+        .iter()
+        .map(|c| {
+            let sign = if c.state { 1 } else { -1 };
+            c.volume() * sign
+        })
+        .sum()
 }
-
 
 #[derive(Debug, Clone)]
-struct Step {
+struct Cube {
     state: bool,
-    min_x: i64,
-    max_x: i64,
-    min_y: i64,
-    max_y: i64,
-    min_z: i64,
-    max_z: i64,
+    x: (i64, i64),
+    y: (i64, i64),
+    z: (i64, i64),
 }
 
-impl Step {
-    fn count(&self) -> i64 {
-        (self.max_x - self.min_x + 1)
-            * (self.max_y - self.min_y + 1)
-            * (self.max_z - self.min_z + 1)
+impl Cube {
+    fn volume(&self) -> i64 {
+        (self.x.1 - self.x.0 + 1) * (self.y.1 - self.y.0 + 1) * (self.z.1 - self.z.0 + 1)
     }
 
-    fn contains(&self, coord: &Coord) -> bool {
-        let x= coord.0;
-        let y= coord.1;
-        let z= coord.2;
-        x >= self.min_x && x <= self.max_x
-            && y >= self.min_y && y <= self.max_y
-            && z >= self.min_z && z <= self.max_z
+    fn sub_cube(&self, other: &Cube) -> Option<Cube> {
+        let x = Cube::sub_edge(self.x, other.x)?;
+        let y = Cube::sub_edge(self.y, other.y)?;
+        let z = Cube::sub_edge(self.z, other.z)?;
+        Some(Cube {
+            state: self.state,
+            x,
+            y,
+            z,
+        })
     }
 
-    fn intersection_with(&self, other: &Step) -> Vec<Coord> {
-        // 8
-        let mut intersections = vec![];
-        for vertex in self.vertexs() {
-            if other.contains(&vertex) {
-                intersections.push(vertex);
-            }
+    fn sub_edge((a, b): (i64, i64), (low, high): (i64, i64)) -> Option<(i64, i64)> {
+        if a > high {
+            return None;
         }
-        intersections
-    }
-
-    fn vertexs(&self) -> [Coord; 8]{
-        [
-            (self.min_x, self.min_y, self.min_z),
-            (self.min_x, self.min_y, self.max_z),
-            (self.min_x, self.max_y, self.min_z),
-            (self.min_x, self.max_y, self.max_z),
-            (self.max_x, self.min_y, self.min_z),
-            (self.max_x, self.min_y, self.max_z),
-            (self.max_x, self.max_y, self.min_z),
-            (self.max_x, self.max_y, self.max_z),
-        ]
-    }
-
-    fn part1_step(&self) -> Self {
-        let min_x = if self.min_x < -50 {
-            -50
-        } else {
-            self.min_x
-        };
-        let max_x = if self.max_x > 50 {
-            50
-        } else {
-            self.max_x
-        };
-        let min_y = if self.min_y < -50 {
-            -50
-        } else {
-            self.min_y
-        };
-        let max_y = if self.max_y > 50 {
-            50
-        } else {
-            self.max_y
-        };
-        let min_z = if self.min_z < -50 {
-            -50
-        } else {
-            self.min_z
-        };
-        let max_z = if self.max_z > 50 {
-            50
-        } else {
-            self.max_z
-        };
-
-        Self { state: self.state , min_x, max_x, min_y, max_y, min_z, max_z }
+        if b < low {
+            return None;
+        }
+        let low = low.max(a);
+        let high = high.min(b);
+        Some((low, high))
     }
 }
 
-impl FromStr for Step {
+impl FromStr for Cube {
     type Err = Box<dyn Error>;
 
     fn from_str(s: &str) -> Result<Self> {
-        let state = if s.starts_with("on") {
-            true
-        } else {
-            false
-        };
+        let state = if s.starts_with("on") { true } else { false };
         let ranges: Vec<&str> = s.split(" ").last().unwrap().split(",").collect();
-        
+
         let range: Vec<&str> = ranges[0][2..].split("..").collect();
         let range: Vec<i64> = range.iter().map(|s| s.parse::<i64>().unwrap()).collect();
-        let (min_x, max_x) = (range[0], range[1]);
-    
+        let x = (range[0], range[1]);
+
         let range: Vec<&str> = ranges[1][2..].split("..").collect();
         let range: Vec<i64> = range.iter().map(|s| s.parse::<i64>().unwrap()).collect();
-        let (min_y, max_y) = (range[0], range[1]);
-    
+        let y = (range[0], range[1]);
+
         let range: Vec<&str> = ranges[2][2..].split("..").collect();
         let range: Vec<i64> = range.iter().map(|s| s.parse::<i64>().unwrap()).collect();
-        let (min_z, max_z) = (range[0], range[1]);
-    
-        Ok(Self { state, min_x, max_x, min_y, max_y, min_z, max_z })
+        let z = (range[0], range[1]);
+
+        Ok(Self { state, x, y, z })
     }
 }
