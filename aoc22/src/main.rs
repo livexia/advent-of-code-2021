2d2683107d8ae4343ac4403e67331e2990c76325
livexia <istillalive@live.cn>
2021-12-22 20:18:45 +0800

    use hashmap to speed up calc

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::io::{self, Read, Write};
 use std::str::FromStr;
+use std::time::{Duration, Instant};
 
 macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
@@ -20,8 +22,27 @@ fn main() -> Result<()> {
 
     writeln!(io::stdout(), "there is {} steps", cuboids.len())?;
 
+    let start = Instant::now();
     part1(&cuboids)?;
+    writeln!(
+        io::stdout(),
+        "Part 1 took {:?} to computer",
+        Instant::now() - start
+    )?;
+    let start = Instant::now();
     part2(&cuboids)?;
+    writeln!(
+        io::stdout(),
+        "Part 2 with stack took {:?} to computer",
+        Instant::now() - start
+    )?;
+    let start = Instant::now();
+    part2_with_hashmap(&cuboids)?;
+    writeln!(
+        io::stdout(),
+        "Part 2 with HashMap took {:?} to computer",
+        Instant::now() - start
+    )?;
     Ok(())
 }
 
@@ -51,6 +72,35 @@ fn part2(cuboids: &Vec<Cuboid>) -> Result<()> {
     Ok(())
 }
 
+fn part2_with_hashmap(cuboids: &Vec<Cuboid>) -> Result<()> {
+    let result = calc_volume_with_hashmap(&cuboids);
+    writeln!(io::stdout(), "Part2: there is {} cubes", result)?;
+    Ok(())
+}
+
+fn calc_volume_with_hashmap(cuboids: &[Cuboid]) -> i64 {
+    let mut counters: HashMap<Cuboid, i64> = HashMap::new(); // 初始化空表，用来存储每次变化之后所有的长方体和长方体出现的次数
+    for next_cuboid in &cuboids[..] {
+        let mut new_counters = counters.clone(); // 复制为新的HashMap，防止遍历HashMap的过程中对其进行修改，导致逻辑错误
+        for (cuboid, count) in counters {
+            // 遍历上一次的长方体
+            if let Some(sub_cuboid) = cuboid.sub_cuboid(next_cuboid) {
+                // 计算重叠区域
+                // 重叠区域的次数为减去当前长方体的次数
+                // 类似于利用栈实现的时候，新的长方体的状态为当前长方体的取反，具体见栈的实现的说明
+                *new_counters.entry(sub_cuboid).or_insert(0) -= count;
+            }
+        }
+        if next_cuboid.state {
+            // 假如输入长方体状态为 on， 直接将表中的长方体的值加一，即出现次数加1
+            *new_counters.entry(next_cuboid.clone()).or_insert(0) += 1;
+        }
+        counters = new_counters;
+    }
+    // 计算总体积的时候，要将长方体的体积乘上长方体出现的次数
+    counters.iter().map(|(c, w)| c.volume() * w).sum()
+}
+
 fn calc_volume(cuboids: &[Cuboid]) -> i64 {
     let mut stack: Vec<Cuboid> = vec![]; // 初始化空栈，用来存储每次变化之后所有的长方体
     for next_cuboid in &cuboids[..] {
@@ -62,13 +112,18 @@ fn calc_volume(cuboids: &[Cuboid]) -> i64 {
             if let Some(mut sub_cuboid) = cuboid.sub_cuboid(next_cuboid) {
                 // 计算当前长方体和输入长方体的重叠区域
                 // 防止累加两次重叠和减去两次重叠
-                if cuboid.state == next_cuboid.state {
-                    // 假如当前长方体和输入长方体的状态一致，重叠长方体的状态应该取反
-                    sub_cuboid.state = !next_cuboid.state;
-                } else {
-                    // 状态不一致时，重叠区域的状态应该和输入长方体的状态一致
-                    sub_cuboid.state = next_cuboid.state;
-                }
+                // if cuboid.state == next_cuboid.state {
+                //     // 假如当前长方体和输入长方体的状态一致，重叠长方体的状态应该取反
+                //     sub_cuboid.state = !next_cuboid.state;
+                // } else {
+                //     // 状态不一致时，重叠区域的状态应该和输入长方体的状态一致
+                //     sub_cuboid.state = next_cuboid.state;
+                // }
+                // 仔细思考这两种情况，可以发现实际上重叠区域的状态，总是和当前长方体的状态相反。
+                // 实际上可以在计算重复区域的时候，直接修改状态，减少需要修改的次数。
+                // 但是在第一部分时，直接计算每一个输入和长方体(-50,-50,-50)、(50,50,50) 的重叠区域作为新的输入
+                // 那么就会导致新的输入和旧的输入状态相反，所以可以不必改动。
+                sub_cuboid.state = !cuboid.state;
                 new_stack.push(sub_cuboid); // 把重叠区域的长方体放入栈中
             }
         }
@@ -87,7 +142,7 @@ fn calc_volume(cuboids: &[Cuboid]) -> i64 {
         .sum()
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Cuboid {
     state: bool,
     x: (i64, i64),
